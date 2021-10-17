1d3bb5969e8a16247f47a89354925c55e400697d
Inksaver <Inksaver@users.noreply.github.com>
2021-10-17 19:44:39 +0100

    Update player.py

--- Python/OOP/00-Adventure Game-layout/player.py
+++ Python/OOP/00-Adventure Game-layout/player.py
@@ -1,28 +1,30 @@
 ''' This is a Python Module equivalent to a C# static class '''
 
 # properties: Can be directly accessed for read/write
-name = ""
-health = 100
-strength = 100
-character = ""
-character_types = []
-inventory = []
+name:str = ""
+health:int = 100
+strength:int  = 100
+character:str = ""
+character_types:list = []
+inventory:list = []
 
 # public methods
-def update_stats(character_index):
+def update_stats(character_index:int) -> None:
 	''' modify health and strength depending on character selected '''
 	pass
 	
-def add_to_inventory(item):
+def add_to_inventory(item:str) -> None:
 	''' add an item to player inventory '''
 	pass
 
-def remove_from_inventory(item):
+def remove_from_inventory(item:str) -> None:
 	''' remove an item from player inventory '''
 	pass
 		
-def display_inventory():
+def display_inventory() -> None:
 	''' display contents of player inventory '''
 	pass
 
-	
+def display_player() -> None:
+	''' main use for debug. prints all player properties '''
+	pass
