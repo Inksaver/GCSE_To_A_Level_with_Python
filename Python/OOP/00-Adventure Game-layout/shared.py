488523d615d971b46f131ef50b650fd13d15f9ed
Inksaver <Inksaver@users.noreply.github.com>
2021-10-17 08:52:58 +0100

    Update shared.py

--- Python/OOP/00-Adventure Game-layout/shared.py
+++ Python/OOP/00-Adventure Game-layout/shared.py
@@ -1,29 +1,24 @@
 '''
-the items and locations are shared by multiple files, so are best kept in this code module
-using dictionaries offers the greatest flexibility
+the items, enemies and locations are shared by multiple files, so are best kept in a code module
+dictionaries of all these objects offer the greatest flexibility
 
-example of items dictionary, which is a dictionary of dictionaries:
-items["torch"] = 
-{
-	"name": "torch",
-	"description": "a flaming wooden torch"
-}
-items["key card"] = 
-{
-	"name": "key card",
-	"description": "Property of Holiday Inn"
-}
-example of locations dictionary which is a dictionary of dictionaries:
-locations["hotel room"] =
-{
-    "name":"hotel room",
-	"description": "a damp hotel room",
-	"to_north":"",
-	"to_east":"coridoor",
-	"to_south":"",
-	"to_west":"",
-	"item_required":"key card"              # key of an item from the items dictionary
-	"items":["torch","plastic sword"]       # a Python list of the dictionary keys of items in the location
+Example of items dictionary, which is a dictionary of item objects
+items["torch"] = <item object>
+
+items["torch"].name -> "torch"
+items["torch"].get_description() = "a flaming wooden torch"
+items["key card"].name" = "key card",
+items["key card"].get_description() = "Property of Holiday Inn"
+
+example of locations dictionary which is a dictionary of location objects
+locations["hotel room"].name = "hotel room"
+locations["hotel room"].description = "a damp hotel room"
+locations["hotel room"].to_north = ""
+locations["hotel room"].to_east = "coridoor"
+locations["hotel room"].to_south = ""
+locations["hotel room"].to_west = ""
+locations["hotel room"].item_required = "key card"              # key of an item from the items dictionary
+locations["hotel room"].items = ["torch","plastic sword"]       # a Python list of the dictionary keys of items in the location
 }
 '''
 items = {}
