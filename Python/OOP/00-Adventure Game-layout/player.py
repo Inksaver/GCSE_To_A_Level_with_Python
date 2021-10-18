4688fb426a3a6f45695bbb8580a014c3bc8b9084
Inksaver <Inksaver@users.noreply.github.com>
2021-10-18 10:25:57 +0100

    Update player.py

--- Python/OOP/00-Adventure Game-layout/player.py
+++ Python/OOP/00-Adventure Game-layout/player.py
@@ -1,30 +1,43 @@
-''' This is a Python Module equivalent to a C# static class '''
+import lib.kboard as kb #note use of lib. to locate file inside a subdirectory called lib
+import player, shared
 
-# properties: Can be directly accessed for read/write
-name:str = ""
-health:int = 100
-strength:int  = 100
-character:str = ""
-character_types:list = []
-inventory:list = []
+def add_to_items(key_name:str, description:str) -> None:
+	''' add a new item dictionary to the shared.items dictionary '''
 
-# public methods
-def update_stats(character_index:int) -> None:
-	''' modify health and strength depending on character selected '''
-	pass
+def add_to_locations(key_name:str, description:str, tonorth:str = '', toeast:str = '', tosouth:str = '', towest:str = '', item_required:str = '', items:list = []) -> None:
+	''' create a new location dictionary to shared.locations '''
+
+def display_intro() -> None:
+	''' Displays an introduction to the adventure '''
 	
-def add_to_inventory(item:str) -> None:
-	''' add an item to player inventory '''
-	pass
+def setup_player() -> None:
+	''' gets player details. Change the text to suit your adventure theme '''
+	
+def setup_items() -> None:
+	''' create items '''
+	
+def setup_world() -> None:
+	''' create locations '''
 
-def remove_from_inventory(item:str) -> None:
-	''' remove an item from player inventory '''
-	pass
+def play() -> None:
+	''' game loop '''
+	
+def display_location(here) -> list:
+	''' descrbe the current location, any items inside it, and exits '''
+	exits:list = []
 		
-def display_inventory() -> None:
-	''' display contents of player inventory '''
-	pass
-
-def display_player() -> None:
-	''' main use for debug. prints all player properties '''
-	pass
+	return exits
+		
+def	take_action(here, exits:list) -> None:
+	''' choose player action '''
+	
+def main() -> None:
+	''' Everything runs from here '''
+	setup_player()			# setup player name and character
+	setup_items()			# create items found in your game
+	setup_world()			# create locations in your game
+	display_intro()			# set the scene for your adventure game
+	play()				# play the game
+	
+main()
+input("Enter to quit")
